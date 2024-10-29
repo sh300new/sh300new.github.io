dbe6492cb0fe1dc23ee0b83c6ec2472afa9e4891
sh300new <sh300new@gmail.com>
2024-10-29 13:58:03 +0900

    d

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,7 +2,9 @@
 function toggleCode() {
   const codeBlock = document.getElementById("code-block");
   const copyBtn = document.querySelector(".copy-btn");
-  if (codeBlock.style.display === "none") {
+
+  // 코드 블록과 복사 버튼이 초기에는 display: "none"이므로 toggle을 위해 정확히 설정합니다.
+  if (codeBlock.style.display === "" || codeBlock.style.display === "none") {
     codeBlock.style.display = "block";
     copyBtn.style.display = "block";
   } else {
